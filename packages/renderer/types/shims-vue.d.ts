03f02887142f9420cbeb373446f3dad2f0f38318
fanly <249932635@qq.com>
2021-05-10 22:29:11 +0800

    增加vuex

--- packages/renderer/types/shims-vue.d.ts
+++ packages/renderer/types/shims-vue.d.ts
@@ -1,5 +1,6 @@
 declare module '*.vue' {
   import type { DefineComponent, ComponentCustomProperties } from 'vue';
+  import type { Store } from 'vuex';
   // eslint-disable-next-line @typescript-eslint/ban-types, @typescript-eslint/no-explicit-any
   const component: DefineComponent<{}, {}, any>;
 
