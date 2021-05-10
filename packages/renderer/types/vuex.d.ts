d6123d60333e6b9de393e130bfeddee115e735a1
fanly <249932635@qq.com>
2021-05-10 22:56:29 +0800

    增加vuex

--- packages/renderer/types/vuex.d.ts
+++ packages/renderer/types/vuex.d.ts
@@ -1,8 +1,7 @@
 // import type { ComponentCustomProperties } from 'vue';
 import type { Store } from '/@/store';
-// eslint-disable-next-line @typescript-eslint/ban-types, @typescript-eslint/no-explicit-any
 declare module '@vue/runtime-core' {
   interface ComponentCustomProperties {
-    $store: Store
+    $store: Store<State>
   }
 }
