c51a32f8191d4ab0a61ddf7b5211c9905e502dd9
fanly <249932635@qq.com>
2021-05-10 22:50:35 +0800

    增加vuex

--- packages/renderer/types/vuex.d.ts
+++ packages/renderer/types/vuex.d.ts
@@ -1,12 +1,8 @@
 // import type { ComponentCustomProperties } from 'vue';
-import type { Store } from 'vuex';
+import type { Store } from '/@/store';
 // eslint-disable-next-line @typescript-eslint/ban-types, @typescript-eslint/no-explicit-any
 declare module '@vue/runtime-core' {
-  interface State {
-    showFestivals: boolean
-  }
-
   interface ComponentCustomProperties {
-    $store: Store<State>
+    $store: Store
   }
 }
