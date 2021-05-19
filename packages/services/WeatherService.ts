a46378feb3b51777944854f9816a8e00e01a716a
fanly <249932635@qq.com>
2021-05-19 23:29:56 +0800

    修改readme，增加功能列表

--- packages/services/WeatherService.ts
+++ packages/services/WeatherService.ts
@@ -4,7 +4,7 @@ import axios from 'axios';
 
 export default class WeatherService {
   async getWeathers() {
-    const res = await axios.get('https://open.coding01.cn/weatherdata');
+    const res = await axios.get('https://***.com/weatherdata');
     return res.data;
   }
 }
